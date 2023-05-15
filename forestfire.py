1f24b2529bb75aeb99f43245dcd1b5e531d8d3a1
falselysium <lancerxancer@gmail.com>
2023-05-15 12:44:54 -0400

    Final Forest Fire model code'

--- forestfire.py
+++ forestfire.py
@@ -8,16 +8,15 @@ from noise import pnoise2
 # Displacements from a cell to its eight nearest neighbours (y, x)
 neighbourhood = (
     (-1, -1), (-1, 0), (-1, 1),
-    ( 0, -1),          ( 0, 1),
-    ( 1, -1), ( 1, 0), ( 1, 1)
-    )
-
-# windp = [.1 if x+y<=0 else 1 for x,y in neighbourhood]
+    (0, -1),          (0, 1),
+    (1, -1), (1, 0), (1, 1)
+)
+# Wind probability for each neighbour
 windp = [
     .1, .1, .1,
-    .1,    .1,
-    .9, .7, .7
-    ]
+    .1,   .1,
+    1, 1, 1]
+
 
 print("Wind Probability", windp)
 EMPTY, TREE, FIRE, WATER = 0, 1, 2, 3
@@ -48,11 +47,12 @@ def iterate(X):
                         break
     return X1
 
+
 # The initial fraction of the forest occupied by trees.
-forest_fraction = 0.2
+forest_fraction = 0.02
 
 # Probability of new tree growth per empty cell, and of lightning strike.
-p, f = 0.03, 0.00005
+p, f = 0.0005, 0.000005
 
 # Forest size (number of cells in x and y directions).
 nx, ny = 150, 150
@@ -64,12 +64,7 @@ X[1:ny-1, 1:nx-1] = np.random.random(size=(ny-2, nx-2)) < forest_fraction
 
 
 # ##########################################################################
-# ##########################################################################
-# # Water
-# # X[10:90, 10:15] = WATER
-# # X[10:90, 40:45] = WATER
-# # X[10:90, 60:65] = WATER
-# # X[10:90, 80:85] = WATER
+# Water Condition using Perlin Noise
 scale = 40    # Controls the frequency of the noise
 octaves = 2   # Controls the detail of the noise
 persistence = 0.3  # Controls the roughness of the noise
@@ -78,26 +73,28 @@ for i in range(150):
     for j in range(150):
         x = i / scale
         y = j / scale
-        noise_val = pnoise2(x, y, 
-                            octaves=octaves, 
-                            persistence=persistence, 
-                            lacunarity=lacunarity, 
-                            repeatx=150, 
+        noise_val = pnoise2(x, y,
+                            octaves=octaves,
+                            persistence=persistence,
+                            lacunarity=lacunarity,
+                            repeatx=150,
                             repeaty=150)
         if noise_val < -0.2:    # Set cells below a certain noise threshold to water
             X[i, j] = 3
 ##########################################################################
-##########################################################################
-            
+
 fig = plt.figure(figsize=(25/3, 6.25))
 ax = fig.add_subplot(111)
 ax.set_axis_off()
 im = ax.imshow(X, cmap=cmap, norm=norm)  # , interpolation='nearest')
 
 # The animation function: called to produce a frame for each generation.
+
+
 def animate(i):
     im.set_data(animate.X)
     animate.X = iterate(animate.X)
+
 
 # Bind our grid to the identifier X in the animate function's namespace.
 animate.X = X
